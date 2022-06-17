ac64cd20987f2b83f7b060a870726e6a98718041
bamsi <hibamsi@gmail.com>
2022-06-17 22:56:44 +0300

    add test for rockets

---
+++ src/redux/__tests__/rockets.test.js
@@ -0,0 +1,60 @@
+import rockets, {
+  cancelReserveRocket,
+  reserveRocket,
+} from "../rockets/rockets.js";
+
+describe("Testing rocket store initial state", () => {
+  test("should return the initial state", () => {
+    expect(rockets(undefined, {})).toEqual({list: []});
+  });
+
+  test("The reducer should return the new state after update", () => {
+    const previousState = {list: []};
+    const data = [
+      {
+        id: 1,
+        name: "The Space x",
+        type: "Rocket",
+        flickr_images: "image1",
+        description: "This is a test rocket",
+        reserved: false,
+      },
+    ];
+    const FETCH_DATA = "spacestore/rockets/FETCH_DATA";
+    const fetchData = (data) => ({ type: FETCH_DATA, data });
+
+    expect(rockets(previousState, fetchData(data))).toEqual({list: [
+      {
+        id: 1,
+        name: "The Space x",
+        type: "Rocket",
+        flickr_images: "image1",
+        description: "This is a test rocket",
+        reserved: false,
+      },
+    ]});
+  });
+
+  test("The value of reserved property should be update to true", () => {
+    const previousState = {list: [
+      {
+        id: 1,
+        name: "The Space x",
+        type: "Rocket",
+        flickr_images: "image1",
+        description: "This is a test rocket",
+        reserved: false,
+      },
+    ]};
+    expect(rockets(previousState,  reserveRocket(1))).toEqual({list: [
+      {
+        id: 1,
+        name: "The Space x",
+        type: "Rocket",
+        flickr_images: "image1",
+        description: "This is a test rocket",
+        reserved: true,
+      },
+    ]});
+  });
+});
