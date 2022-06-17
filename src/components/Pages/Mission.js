ee25a3367de016685bbf7fa0d294cd4df375390b
Michael Ugochukwu <mark.omulu@gmail.com>
2022-06-17 20:06:53 +0100

    Finish testing mission component

--- src/components/Pages/Mission.js
+++ src/components/Pages/Mission.js
@@ -15,67 +15,72 @@ const Missions = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     // @ts-ignore
-    if (missions.length === 0) {
-      dispatch(getAllMissions());
-    }
+    if (!missions.length) dispatch(getAllMissions());
   }, []);
-
-  if (missions.length) {
-    return (
-      <section className={classes.missionsContainer}>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Mission</th>
-              <th>Description</th>
-              <th>Status</th>
-              <th>&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            {missions.map((mission) => (
-              <tr key={mission.id}>
-                <td>{mission.name}</td>
-                <td>{mission.description}</td>
-                <td>
-                  {!mission.joined && <span className={`${classes.status} ${classes['non-member']}`}> Not a member </span>}
-                  {mission.joined && <span className={`${classes.status} ${classes['active-member']}`}> Active member </span>}
-                </td>
-                <td>
-                  {!mission.joined && (
-                    <button
-                      type="button"
-                      className={`${classes.missionBtn} ${classes['join-button']}`}
-                      onClick={() => {
-                        dispatch(joinMission(mission.id));
-                      }}
-                    >
-                      Join Mission
-                    </button>
-                  )}
-                  {mission.joined && (
-                    <button
-                      type="button"
-                      className={`${classes.missionBtn} ${classes['leave-button']}`}
-                      onClick={() => {
-                        dispatch(leaveMission(mission.id));
-                      }}
-                    >
-                      Leave Mission
-                    </button>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </section>
-    );
-  }
   return (
-    <>
-      <p>There are no missions </p>
-    </>
+    <section className={classes.missionsContainer}>
+      <h2 className={classes.header}>Missions Page</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Mission Name</th>
+            <th>Mission Description</th>
+            <th>Mission Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.id}>
+              <td>{mission.name}</td>
+              <td>{mission.description}</td>
+              <td>
+                {!mission.joined && (
+                  <span
+                    className={`${classes.status} ${classes['non-member']}`}
+                  >
+                    {' '}
+                    Not a member
+                    {' '}
+                  </span>
+                )}
+                {mission.joined && (
+                  <span
+                    className={`${classes.status} ${classes['active-member']}`}
+                  >
+                    {' '}
+                    Active member
+                    {' '}
+                  </span>
+                )}
+              </td>
+              <td>
+                {!mission.joined && (
+                  <button
+                    type="button"
+                    className={`${classes.missionBtn} ${classes['join-button']}`}
+                    onClick={() => dispatch(joinMission(mission.id))}
+                  >
+                    Join Mission
+                  </button>
+                )}
+                {mission.joined && (
+                  <button
+                    type="button"
+                    className={`${classes.missionBtn} ${classes['leave-button']}`}
+                    onClick={() => {
+                      dispatch(leaveMission(mission.id));
+                    }}
+                  >
+                    Leave Mission
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </section>
   );
 };
 export default Missions;
