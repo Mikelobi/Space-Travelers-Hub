a2f861d64b5d676c8194df41e9d25a3913952d90
Michael Ugochukwu <mark.omulu@gmail.com>
2022-06-17 15:49:43 +0100

    Rendered list of joined mission to my profile page.

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -1,8 +1,10 @@
 import ReservedRocket from '../rockets/reservedRocket';
+import JoinedMission from './joinedMission';
 
 const Profile = () => (
   <div>
     <ReservedRocket />
+    <JoinedMission />
   </div>
 );
 export default Profile;
